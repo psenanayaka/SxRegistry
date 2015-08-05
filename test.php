c2ea9dd05f7f31ad1cc41963d8f051f8834b8cca
psenanayaka <p_senanayaka@yahoo.com>
2015-08-05 11:08:25 +0530

    by priyanga for testing

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+All Rights Reserved
+(c) Priyanga Senanayaka 
+2014
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo 'Its working fine!'
+        ?>
+    </body>
+</html>
